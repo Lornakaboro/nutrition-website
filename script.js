693bf39afc37565b5e7dcfdee61e71ce0bc16647
Lornakaboro <lornakaboro@gmail.com>
2022-12-03 23:17:48 +0300

    Fix linter errors

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* eslint no-unused-vars: 0 */
+
 function toggleMobileMenu() {
   document
     .getElementById('nav-dropdown-menu')
@@ -18,50 +20,49 @@ function openDropdownItem(menuElement, menuLink) {
 }
 
 const specialistData = [
-{
-  featuredImage: './images/Mary-Kaboro.jpg',
-  name: 'Dr Mary Kaboro pHD.',
-  specialization: 'Senior Nutritionist',
-  description:
+  {
+    featuredImage: './images/Mary-Kaboro.jpg',
+    name: 'Dr Mary Kaboro pHD.',
+    specialization: 'Senior Nutritionist',
+    description:
     'Nationally acclaimed and award winning clinical nutritionist.Teaches human nutrition at the university of Nairobi',
-  
-  
-},
-{
-  featuredImage: './images/Lactation expert.jpg',
-  name: 'Lorna Nyawira',
-  specialization: 'Lactation Expert',
-  description:
+
+  },
+  {
+    featuredImage: './images/Lactation expert.jpg',
+    name: 'Lorna Nyawira',
+    specialization: 'Lactation Expert',
+    description:
     'Certified breastfeeding and lactation specialist.Authored the book Breastfeeding is Life 2009.',
-},
-{
-  featuredImage: './images/fitness -coach.jpg',
-  name: 'Ian Kirii',
-  specialization: 'Fitness and wellness coach',
-  description:
+  },
+  {
+    featuredImage: './images/fitness -coach.jpg',
+    name: 'Ian Kirii',
+    specialization: 'Fitness and wellness coach',
+    description:
     'Lifestyle and behaviour change specialist,excecutive director YOLO,Fitness center.',
-},
-{
-  featuredImage: './images/Senior dietician.jpg',
-  name: 'Denis Munyao',
-  specialization: 'Senior Dietician,director Meal plans by Deno',
-  description:
+  },
+  {
+    featuredImage: './images/Senior dietician.jpg',
+    name: 'Denis Munyao',
+    specialization: 'Senior Dietician,director Meal plans by Deno',
+    description:
     'Nutrition specialist,founder of the campaign You are what you eat 2020.',
-},
-{
-  featuredImage: './images/pharmacist.jpg',
-  name: 'Albert Johnson',
-  specialization: 'Certified pharmacist',
-  description:
+  },
+  {
+    featuredImage: './images/pharmacist.jpg',
+    name: 'Albert Johnson',
+    specialization: 'Certified pharmacist',
+    description:
     'Registered pharmacist specializing in  a broad spectrum of nutritional supplements.',
-},
-{
-  featuredImage: './images/Kevin-bahati.jpg',
-  name: 'Kevin Bahati',
-  specialization: 'Registered Nutritionist',
-  description:
+  },
+  {
+    featuredImage: './images/Kevin-bahati.jpg',
+    name: 'Kevin Bahati',
+    specialization: 'Registered Nutritionist',
+    description:
     'Registered nutritionist specializing in nutrional assesments,diagonosis and evaluations.',
-},
+  },
 ];
 
 for (let i = 0; i < specialistData.length; i += 1) {
